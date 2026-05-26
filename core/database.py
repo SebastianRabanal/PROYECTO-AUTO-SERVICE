17207b9d13fbc4af05d46da60f46f43224ec7a10
Sebastian <sebastianrabanal34@gmail.com>
2026-05-25 23:10:19 -0500

    Vinculando con la Base de datos de la nube

--- core/database.py
+++ core/database.py
@@ -17,6 +17,7 @@ class Database:
         try:
             conexion = pymysql.connect(
                 host=os.getenv("DB_HOST", "localhost"),
+                port=int(os.getenv("DB_PORT", 3306)),
                 user=os.getenv("DB_USER", "root"),
                 password=os.getenv("DB_PASSWORD", ""),
                 database=os.getenv("DB_NAME", "autoservice"),
